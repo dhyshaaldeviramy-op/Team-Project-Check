85cf2efe284cf60adf1fc783763ef6ca27ef9faf
Dhyshaal Devira <dhyshaaldevira@gmail.com>
2026-04-06 22:11:33 +0530

    Added Backend project with Database

---
+++ Team-Project-Check/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Team_Project_Check.Model;
+
+namespace Team_Project_Check.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
